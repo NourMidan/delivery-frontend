e112822ae0572ad7750539590b6c009e3303a31f
unknown <nourmidan67@gmail.com>
2023-01-02 18:49:04 +0200

    logout

--- components/nav.tsx
+++ components/nav.tsx
@@ -92,7 +92,7 @@ const Nav = (props: { auth: Auth }) => {
       onClick={() => handleLogut()}
       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
     >
-      logut
+      Logut
     </button>
   );
 
